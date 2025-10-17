ddf2e3842f2eae99200e521bccc31cb57d9bc39c
Sadhiq93 <116905354+Sadhiq93@users.noreply.github.com>
2025-10-17 23:46:30 +0530

    Update task-02-answer.py

--- Day-06/02-Assignment/02-Answers/task-02-answer.py
+++ Day-06/02-Assignment/02-Answers/task-02-answer.py
@@ -14,3 +14,26 @@ print("a <= b:", less_than_or_equal)
 print("a >= b:", greater_than_or_equal)
 print("a == b:", equal)
 print("a != b:", not_equal)
+
+---
+a = 10
+b = 20
+
+greater = a>b
+less = a<b
+equal = a==b
+lessEqual = a<=b
+greaterEqual = a>=b
+notEqual = a!=b
+
+print(greater, less, equal, lessEqual, greaterEqual, notEqual)
+
+if greater:
+  print(a, b)
+else:
+  print("a is less than b")
+
+---
+output:
+False True False True False True
+a is less than b
