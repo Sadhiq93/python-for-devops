1c565601d1a7fe654e674aa671c9aae5d55ca309
Sadhiq93 <116905354+Sadhiq93@users.noreply.github.com>
2025-10-18 00:12:32 +0530

    Update task-05-answer.py

--- Day-06/02-Assignment/02-Answers/task-05-answer.py
+++ Day-06/02-Assignment/02-Answers/task-05-answer.py
@@ -15,3 +15,27 @@ print("a is my_list:", is_same_object)
 print("b is not my_list:", is_not_same_object)
 print("3 in my_list:", element_in_list)
 print("6 not in my_list:", element_not_in_list)
+-------
+
+my_list = ["sam", "mahe", "sid", "lachi", "mustafa"]
+
+my_copy = my_list
+global_list = []
+
+is_same_object = my_copy is my_list
+is_not_same_object = global_list is my_list
+
+element_in_list = "sam" in my_list
+element_not_in_list = "navya" in my_list
+
+print("list is my_list:", is_same_object)
+print("global_list is my_list:", is_not_same_object)
+print("sam in my_list:", element_in_list)
+print("navya in my_list:", element_not_in_list)
+------
+output:
+
+list is my_list: True
+global_list is my_list: False
+sam in my_list: True
+navya in my_list: False
